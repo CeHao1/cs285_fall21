fe56d944cc644deb82e30e597149bdc7d84e26f1
ce-hao <ce.hao@tusimple.ai>
2021-09-16 10:43:15 +0800

    copy from hw1

--- hw2/cs285/policies/MLP_policy.py
+++ hw2/cs285/policies/MLP_policy.py
@@ -87,6 +87,8 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
     # query the policy with observation(s) to get selected action(s)
     def get_action(self, obs: np.ndarray) -> np.ndarray:
         # TODO: get this from HW1
+        action = self.forward(ptu.from_numpy(observation))
+        return  ptu.to_numpy(action)
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
