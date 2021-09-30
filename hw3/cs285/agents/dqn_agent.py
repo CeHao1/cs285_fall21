a8cb2ff0644747378e0f1a8ac8a14d8cd6c92684
ce-hao <ce.hao@tusimple.ai>
2021-09-30 13:29:08 +0800

    finish dqn agent

--- hw3/cs285/agents/dqn_agent.py
+++ hw3/cs285/agents/dqn_agent.py
@@ -100,7 +100,7 @@ class DQNAgent(object):
 
             # TODO fill in the call to the update function using the appropriate tensors
             log = self.critic.update(
-                ob_no, ac_na, next_ob_no, reward_n, terminal_n
+                ob_no, ac_na, next_ob_no, re_n, terminal_n
             )
 
             # TODO update the target network periodically 
